3e733b94788b093ceda4bd5ae83ded6c62537b2c
davidnoam4 <69966679+davidnoam4@users.noreply.github.com>
2022-03-01 12:51:49 +0200

    test for: - Cylinder - Plane - Polygon - Sphere

--- unittests/geometries/CylinderTest.java
+++ unittests/geometries/CylinderTest.java
@@ -1,12 +1,20 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Unit tests for Cylinder class
+ *
+ */
 class CylinderTest {
 
+    /**
+     * Test method for {@link Vector getNormal(Point)}.
+     */
     @Test
-    void getNormal() {
+    void testGetNormal() {
     }
 }
--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -1,12 +1,21 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Unit tests for Plane class
+ *
+ */
 class PlaneTest {
 
+    /**
+     * Test method for {@link Vector getNormal(Point)}.
+     */
     @Test
-    void getNormal() {
+    void testGetNormal() {
     }
 }
